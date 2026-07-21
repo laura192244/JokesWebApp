e7fb01616fb4f43e86753ace2986d1b429c2a272
Laura <misakyanlaura@gmail.com>
2026-07-21 12:55:13 +0400

    commit

---
+++ Views/Jokes/ShowSearchForm.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JokesWebApp.Views.Jokes
+{
+    public class ShowSearchFormModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
